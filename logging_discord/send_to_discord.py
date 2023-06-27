c1d391cf9dd7bd4ec7706ed9aac0d0339af006c3
bruno_brown <brunobrown.86@gmail.com>
2023-06-27 16:51:49 -0300

    removido arquivo com dicionario de logs

--- logging_discord/send_to_discord.py
+++ logging_discord/send_to_discord.py
@@ -1,7 +1,6 @@
 import logging
 import traceback
 import httpx
-from data_log import DATA_LOG
 
 LOG_LEVELS = {
     'unknown': 0,
@@ -11,6 +10,21 @@ LOG_LEVELS = {
     'error': 4,
     'critical': 5
 }
+
+DATA_LOG = [
+    #   color legend:
+    #   * 2040357 = Black
+    #   * 8947848 = Gray
+    #   * 2196944 = Blue
+    #   * 16497928 = Yellow
+    #   * 14362664 = Red
+    {"emoji": ":thinking:   ", "title": "UNKNOWN ERROR", "color": 2040357},
+    {"emoji": ":bug:   ", "title": "DEBUG", "color": 8947848},
+    {"emoji": ":information_source:   ", "title": "INFO", "color": 2196944},
+    {"emoji": ":warning:   ", "title": "WARNING", "color": 16497928},
+    {"emoji": ":x:   ", "title": "ERROR", "color": 14362664},
+    {"emoji": ":sos:   ", "title": "CRITICAL", "color": 14362664}
+]
 
 
 class LogDiscord:
